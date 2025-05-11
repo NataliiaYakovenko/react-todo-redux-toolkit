c153437c4b6db514a03a06b4bdcff63031d84b35
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-05-11 15:24:30 +0300

    Add addTask to todoSlice

--- src/store/slices/todoSlice.js
+++ src/store/slices/todoSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { v4 as uuidv4 } from "uuid";
 
 const initialState = {
   arrayTask: [],
@@ -10,9 +11,23 @@ const todoSlice = createSlice({
   initialState,
   name: "todo",
   reducers: {
-    
+    addTask: (state) => {
+      const newObjectTask = {
+        id: uuidv4(),
+        text: state.newTask,
+        completed: false,
+      };
+      state.arrayTask.push(newObjectTask);
+      state.newTask = "";
+      state.error = "";
+    },
+
+   
+
+
   },
-
-
-
 });
+
+const {reducer,actions}= todoSlice
+
+export default reducer
