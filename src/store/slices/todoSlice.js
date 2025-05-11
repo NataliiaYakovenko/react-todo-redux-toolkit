d29bd9bdc7c12d3914f9a55966187279d7364db9
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-05-11 14:35:27 +0300

    Add todoSlice in to slices

---
+++ src/store/slices/todoSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  arrayTask: [],
+  newTask: "",
+  error: "",
+};
+
+const todoSlice = createSlice({
+  initialState,
+  name: "todo",
+  reducers: {
+    
+  },
+
+
+
+});
