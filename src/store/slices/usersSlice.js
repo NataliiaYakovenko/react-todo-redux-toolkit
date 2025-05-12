3f0bc5edc57056e04f1ef1be3bcc100ab839de9e
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-05-12 22:23:38 +0300

    Add usersSlice

---
+++ src/store/slices/usersSlice.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import getUsers from "../../api/apiFooterUsers";
+
+const initialState = {
+  users: [],
+  isFetching: false,
+  error: null,
+};
+
+export const getUsersThunk = createAsyncThunk(
+  "users/getUsers",
+  async (thuncApi) => {
+    try {
+      const { data } = await getUsers();
+      return data;
+    } catch (error) {
+      return thuncApi.rejectWithValue({ message: error.message });
+    }
+  }
+);
+
+const usersSlice = createSlice({
+  initialState,
+  name: "users",
+  reducers: {},
+  extraReducers: (bulder) => {
+    bulder.addCase(getUsersThunk.pending, (state) => {
+      state.isFetching = true;
+      state.error = null;
+    });
+    bulder.addCase(getUsersThunk.fulfilled, (state, { payload }) => {
+      state.users = payload;
+      state.isFetching = false;
+    });
+    bulder.addCase(getUsersThunk.rejected, (state, { payload }) => {
+      state.isFetching = false;
+      state.error = payload;
+    });
+  },
+});
+
+const { reducer } = usersSlice;
+export default reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./slices/todoSlice";
 import weatherReducer from "./slices/weatherSlice";
+import usersReducer from'./slices/usersSlice'
 
 const store = configureStore({
   reducer: {
     todo: todoReducer,
     weather: weatherReducer,
-    // users: usersReducer
+    users: usersReducer
   },
 });
 
